a4f54ccba4031c2725296382353e6b39b72a34a0
yugchoubey12 <167456445+yugchoubey12@users.noreply.github.com>
2024-11-11 17:13:59 +0530

    Update input.tsx

--- frontend/src/components/input.tsx
+++ frontend/src/components/input.tsx
@@ -1,30 +1,24 @@
-// frontend/src/components/Input.tsx
 import React from "react";
 
-type InputProps = {
+interface InputProps {
   name: string;
   placeholder: string;
   handleChange: React.ChangeEventHandler<HTMLInputElement>;
   type: string;
-  className?: string;
-};
+  className: string;
+}
 
-const Input: React.FC<InputProps> = ({
-  name,
-  placeholder,
-  handleChange,
-  type,
-  className,
-}) => {
+const Input = ({ name, placeholder, handleChange, type, className }: InputProps) => {
   return (
     <input
-      type={type}
       name={name}
       placeholder={placeholder}
       onChange={handleChange}
-      className={`p-2 rounded-lg focus:outline-none ${className}`}
+      type={type}
+      className={className}
     />
   );
 };
 
 export default Input;
+
